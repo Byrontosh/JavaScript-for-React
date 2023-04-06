614efa56333dcfc3d85b66f15f728cd85962def3
Byrontosh <by_tosh20@hotmail.com>
2023-04-06 18:05:33 -0500

    Modules

---
+++ fundaments.js
@@ -0,0 +1,38 @@
+// const login = require('./module')
+// login()
+
+
+
+
+
+// const {login, LIKES} = require('./module')
+// login()
+// console.log(LIKES);
+
+
+
+
+
+// const {login, LIKES} = require('./module')
+// login()
+// console.log(LIKES);
+
+
+
+
+
+
+
+// import login from "./module.js";
+// login()
+
+
+
+// import {login} from "./module.js";
+// login()
+
+
+
+
+// import {login} from "./module.js";
+// login()
---
+++ module.js
@@ -0,0 +1,66 @@
+// login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
+// module.exports = login
+
+
+
+
+
+
+
+
+// login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
+// const LIKES = 150
+// module.exports = {
+//     login,
+//     LIKES
+// }
+
+
+
+
+
+
+
+// module.exports.login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
+// module.exports.LIKES = 150
+
+
+
+
+
+
+
+// const login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
+// export default login
+
+
+
+
+
+// const login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
+// export {
+//     login
+// }
+
+
+
+
+// export const login = (username,password)=>{
+//     if(!username && !password) return console.log("You are not autorized")
+//     console.log(`Welcome ${username}`)
+// }
